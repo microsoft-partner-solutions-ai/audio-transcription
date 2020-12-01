7fa309a01618d1ecf65e6c9ed82e130e47a5713f
Devanshi Thakar <dthakar@microsoft.com>
2020-11-30 18:58:04 -0800

    removed repeating char,words,brkts

--- src/preprocess.py
+++ src/preprocess.py
@@ -46,26 +46,43 @@ def main(args):
 
         # regex to remove repeating characters more than four times 
         regex_repeat_char = r'(.)\1+'
-        subst_repear_char = "\\1\\1"
+        subst_repeat_char = "\\1\\1\\1"
 
+        # regex to remove repeating words more than four times
+        regex_repeat_words = r'(\b(\w+)\b)(\s\1+){3,}'
+        subst_repeat_words = "\\1 \\1 \\1"
+
+        # regex to remove [] comments with timestamp
+        regex_brk_time = r'(\[\w+)(?:\s\d{2}:\d{2}:\d{2})(\])'
+        subst_brk_time = ""
+
+        # regex to remove [] comments without time
+        regex_brk = r'(\[\w+\])'
+        subst_brk = ""
+
+        # regex to remove () comments
+        regex_paren = r'(\(\w+\))'
+        subst_paren = ""
+        
         # list to store the utterances 
         txt = []
-
+        
         for line in text:
                 # remove speakers and timestamps
                 line = re.sub(regex_rem_speaker_time, subst_speaker_time, line)
-
+                
                 # lowercase
                 line = line.lower()
 
                 # replace new line with empty space
                 line = ' '.join(line.split())
+             
+                # removing the [] comments with timestamp and without
+                line = re.sub(regex_brk_time, subst_brk_time, line, re.MULTILINE)
+                line = re.sub(regex_brk, subst_brk, line)
 
-                # avoid repeating characters more than 4 times
-
-                # avoid repeating words more than 4 times 
-
-                # removing the [inaudible] comments
+                # removing the () comments 
+                line = re.sub(regex_paren, subst_paren, line)
 
                 # remove punctuation
                 line = remove_punctuation(line)
@@ -79,6 +96,12 @@ def main(args):
 
         # join list of text into single string
         txt = ' '.join(txt)
+
+        # avoid repeating words more than 4 times 
+        txt = re.sub(regex_repeat_words, subst_repeat_words, txt, re.MULTILINE)
+
+        # avoid repeating characters more than 4 times 
+        txt = re.sub(regex_repeat_char, subst_repeat_char, txt, re.MULTILINE)
 
         # save output
         out_file.write(txt)
